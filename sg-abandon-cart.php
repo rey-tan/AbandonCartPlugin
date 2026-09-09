1b991db3927936c95e6b8d443704246c911be7dd
reytan <rehanboyaju523@gmail.com>
2026-09-09 11:09:55 +0545

    First commit

---
+++ sg-abandon-cart.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+* Plugin Name: SG Abandon Cart
+ * Plugin URI: https://splendourgroup.org/
+ * Description: Woocommerce Cart Abandon Plugin
+ * Version: 1.0.0
+ * Author: Splendour Group
+ * Requires Plugins: woocommerce
+ * Author URI: https://splendourgroup.org/
+ * Text Domain: sg-abandon-cart
+ */
+
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+add_action('plugins_loaded', function(){
+
+    if (!class_exists('WooCommerce')) {
+
+        add_action('admin_notices', function(){
+
+            echo '<div class="notice notice-error">
+                <p>SG Abandon Cart requires WooCommerce.</p>
+            </div>';
+
+        });
+
+        return;
+    }
+
+});
+
+
+//loads the files inside include in the current plugin directory
+require_once plugin_dir_path(__FILE__) . 'includes/functions.php';
+
+
+
+//load files if in admin dashboard
+if(is_admin()){
+    require_once plugin_dir_path(__FILE__) . 'admin/sg-abandon-cart-admin.php';
+}
+
+
+/**
+ * Run on plugin activation
+ */
+register_activation_hook(__FILE__,function(){
+    error_log("SG Abandon Cart plugin activated");
+});
+
+
+/**
+ * Run on plugin deactivation
+ */ 
+register_deactivation_hook(__FILE__,function(){
+    error_log("SG Abandon Cart plugin deactivated");
+});
